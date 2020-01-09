40b44a8ba4455ad3a248dd827593374e494b2e1f
Theyme <themiladeyopaleke@gmail.com>
2020-01-09 09:32:03 -0500

    saving data

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String dogName;
+    private String dogAge;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDogName() {
+        return dogName;
+    }
+
+    public void setDogName(String dogName) {
+        this.dogName = dogName;
+    }
+
+    public String getDogAge() {
+        return dogAge;
+    }
+
+    public void setDogAge(String dogAge) {
+        this.dogAge = dogAge;
+    }
+}
